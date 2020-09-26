dc824153e491e1ce05666fee3c4258e2a9c928eb
Alexey Mitroshin <lapadaviloff@yandex.ru>
2020-09-26 19:27:03 +0300

    to git

--- boids/boids.cpp
+++ boids/boids.cpp
@@ -1,9 +1,17 @@
-﻿#include <SFML/Graphics.hpp>
+﻿/*
+ Mitroshin Aleksey
+lapadaviloff@yandex.ru
+(miam.devsoft@gmail.com)
+алгоритм движения юнитов)))
+ver 1.0
+
+исходный код,с которого все началось: 
+FamTrinli: Asteroids   https://www.youtube.com/watch?v=rWaSo2usU4A&t=24s
+*/
+#include <SFML/Graphics.hpp>
 #include <time.h>
 #include <list>
-#include <sstream>
 #include<iostream>
-#include <cmath>
 using namespace sf;
 
 const int W = 1200;
@@ -17,9 +25,7 @@ public:
     float Frame, speed;
     Sprite sprite;
     std::vector<IntRect> frames;
-
     Animation():Frame (0),speed (0) {}
-
     Animation(Texture& t, int x, int y, int w, int h, int count, float Speed)
     {
         Frame = 0;
@@ -32,7 +38,6 @@ public:
         sprite.setOrigin(w / 2, h / 2);
         sprite.setTextureRect(frames[0]);
     }
-
 
     void update()
     {
@@ -48,7 +53,6 @@ public:
     }
 
 };
-
 
 class Entity
 {
@@ -78,16 +82,6 @@ public:
         anim.sprite.setPosition(x, y);
         anim.sprite.setRotation(angle + 90);
         app.draw(anim.sprite);
-        /*
-        CircleShape circle(R);
-        circle.setFillColor(Color(255, 0, 0, 170));
-        circle.setPosition(x, y);
-        circle.setOrigin(R, R);
-        app.draw(circle);
-
-    */
-
-
     }
 
     virtual ~Entity() {};
@@ -104,31 +98,16 @@ public:
         y1=rand() % H;
    
     }
-
     void  update()
     {
-      
       if(!isCollide)angle = (atan2(x1 - x, y - y1))*57.2956/6;
-      angle += (rand() % 7 - 3)* DEGTORAD;  /*try this*/
-
-      //   angle = angle * DEGTORAD;
-     
-      /* 
-        if (isCollide) { 
-           // angle += 3.14;
-            angle = -angle;
-            isCollide = false; 
-            //std::cout << "check" << std::endl;
-        }
-        */ 
-
-      
+      angle += (rand() % 7 - 3)* DEGTORAD; //отклонение траектории
+      //   angle = angle * DEGTORAD; //первод в радианы
       if (isCollideFuture) {
           if (x > W) {
               angle = angle + 3.14;
               x = W;
-          }
-            
+          } 
           if (x < 0) {
               angle = angle + 3.14;
               x = 0;
@@ -142,47 +121,29 @@ public:
               y = 0;
           }
           isCollideFuture = false;
-
       }
       else {
-          if (x > W) angle = 3.14 - angle;
-          if (x < 0) angle = 3.14 - angle;
-          if (y > H) angle = -angle;
-          if (y < 0) angle = -angle;
+          if (x > W) {
+              angle = 3.14 - angle;
+              x = W;
+          }
+          if (x < 0) {
+              angle = 3.14 - angle;
+              x = 0;
+          }
+          if (y > H) {
+              angle = -angle;
+              y = H;
+          }
+          if (y < 0) {
+               angle = -angle;
+               y = 0;
+          }
       }
-        
         dx = cos(angle)*6 ;
         dy = sin(angle)*6 ;
-        
-        //std::cout << dx << " " << dy << std::endl;
-
-        /*
-       if ((x + dx) > 50 && (x + dx) <( W-50)) x += dx;
-       if ((y + dy) > 50 && (y + dy) < (H-50)) y += dy;
-       */
         x += dx;
         y += dy;
-        //if (x > W || x<0 || y>H || y < 0) life = 0;
-        /*
-         if (x > W) x = 0;  if (x < 0) x = W;
-        if (y > H) y = 0;  if (y < 0) y = H;
-
-        */
-        
-          
-          //  angle = angle * 57.2956;
-        
-        /*
-        if (x > W || x<0 || y>H || y < 0) {
-            if (angle < 90) angle = 180 - angle;
-            else
-                if (angle < 180)angle = 270 - (angle - 90);
-                    else
-                    if (angle < 270)angle = 360 - (angle - 180);
-                    else angle = 180 - (360 - angle);
-        }
-        */
-        
     }
 
     void draw(RenderWindow& app)
@@ -190,7 +151,6 @@ public:
         anim.sprite.setPosition(x, y);
         anim.sprite.setRotation(angle * 57.2956 + 90); // отрисовка спрайта (перевод с радиан в градусы)
         app.draw(anim.sprite);
-    
    /*
    
     CircleShape circle(2);
@@ -198,24 +158,17 @@ public:
     circle.setPosition(x, y);
     circle.setOrigin(R, R);
     app.draw(circle);   
-  */
-            
+  */       
     }
-
-
 };
 
 
 bool isCollideFuture(Entity* a, Entity* b)// пересечение в будующем
 {
 
-    
     return (b->x - a->x) * (b->x - a->x) +
         (b->y - a->y) * (b->y - a->y) <
         ((a->R+6)+ (b->R+6)) * ((a->R+6) + (b->R+6));
-        
-   
-
         }
 
 bool isCollide(Entity* a, Entity* b)  // пересечение
@@ -224,7 +177,7 @@ bool isCollide(Entity* a, Entity* b)  // пересечение
     return (b->x - a->x) * (b->x - a->x) +
         (b->y - a->y) * (b->y - a->y) <
         (a->R + b->R) * (a->R + b->R) - 1;
-        */
+      */  
     return abs(a->x - b->x) < a->R && abs(a->y - b->y) < a->R;
 }
 float isMidlle(Entity* a, Entity* b) {
@@ -237,92 +190,43 @@ float calcCoordinate(float &a, float &b) { // куда лететь
 int main()
 {
     float x1, x2, y1, y2,minDistance;
-    Font font;//шрифт 
-    font.loadFromFile("CyrilicOld.TTF");
-    Text text("", font, 20);
-    text.setFillColor(Color::Red);//покрасили текст в красный. если убрать эту строку, то по умолчанию он белый
-   // text.setStyle(Text::Bold);
-    text.setPosition(0, 0);
-    text.setCharacterSize(24);
-
-
-
+    
     srand(time(0));
 
     RenderWindow app(VideoMode(W, H), "boids");
     app.setFramerateLimit(60);
 
-    Texture t1, t2, t3, t4, t5, t6, t7;
-
-    t1.loadFromFile("images/spaceship.png");
+    Texture t1, t2;
+    
     t2.loadFromFile("images/background.jpg");
-    t3.loadFromFile("images/explosions/type_C.png");
-    t4.loadFromFile("images/rock.png");
-    t5.loadFromFile("images/fire_blue.png");
-    t6.loadFromFile("images/rock_small.png");
-    t7.loadFromFile("images/explosions/type_B.png");
-
-    t1.setSmooth(true);
-    t2.setSmooth(true);
+  
+    t1.loadFromFile("images/fire_blue.png");
 
     Sprite background(t2);
-
-    Animation sExplosion(t3, 0, 0, 256, 256, 48, 0.5);
-    Animation sRock(t4, 0, 0, 64, 64, 16, 0.2);
-    Animation sRock_small(t6, 0, 0, 64, 64, 16, 0.2);
-    Animation sBullet(t5, 0, 0, 32, 64, 16, 0.8);
-    Animation sPlayer(t1, 40, 0, 40, 40, 1, 0);
-    Animation sPlayer_go(t1, 40, 40, 40, 40, 1, 0);
-    Animation sExplosion_ship(t7, 0, 0, 192, 192, 64, 0.5);
-
-
+   
+   Animation sBullet(t1, 0, 0, 32, 64, 16, 0.8);
+  
     std::list<Entity*> entities;
     
     Entity* tempBird = new bird();
-    /*
-    bird* a = new bird();
-    a->settings(sBullet, rand() % W, rand() % H, rand() % 360, 25);
-    entities.push_back(a);
-    */
-    for (int i = 0; i< 200;i++)
+  
+    for (int i = 0; i< 3;i++)
     {
         bird* a = new bird();
-        a->settings(sBullet, rand() % W, rand() % H, rand() % 360, 25);
+        a->settings(sBullet, rand() % W, rand() % H, rand() % 360, 100);
         entities.push_back(a);
     }
    
-  
-
     /////main loop/////
     while (app.isOpen())
     {
+        
         Event event;
         while (app.pollEvent(event))
         {
             if (event.type == Event::Closed)
-                app.close();
-            /*
-            if (event.type == Event::KeyPressed)
-                if (event.key.code == Keyboard::Space)
-                {
-                    bullet* b = new bullet();
-                    b->settings(sBullet, p->x, p->y, p->angle, 10);
-                    entities.push_back(b);
-                }*/
+                app.close();     
         }
-       /* if (Keyboard::isKeyPressed(Keyboard::Space))
-        {
-            bullet* b = new bullet();
-            b->settings(sBullet, p->x, p->y, p->angle, 10);
-            entities.push_back(b);
-        }
-        */
-        /*
-        if (Keyboard::isKeyPressed(Keyboard::Right)) p->angle += 3;
-        if (Keyboard::isKeyPressed(Keyboard::Left))  p->angle -= 3;
-        if (Keyboard::isKeyPressed(Keyboard::Up)) p->thrust = true;
-        else p->thrust = false;
-        */
 
         for (auto a : entities) {
             x2 = a->x;
@@ -340,19 +244,10 @@ int main()
                     tempBird = b;
    
                 }
-               // if (isCollide(a, b))std::cout << 1 << std::endl;
+             
             }
             if ((a->x == tempBird->x) || (a->y == tempBird->y))continue;
             if (isCollide(a, tempBird)) {
-               // std::cout << "x = "<<a->x<<" x ="<<tempBird->x << "y = " << a->y << " y =" << tempBird->y<<std::endl;
-                /*
-                if (a->angle > tempBird->angle) {
-                    a->angle = tempBird->angle - (rand()%1+17)/100; // выбор отклонения
-                }
-                else {
-                    a->angle = tempBird->angle + (rand() % 1 + 17) / 100;
-                }
-            */
                 a->isCollide = true;
                 if (a->x+a->y > tempBird->x+ tempBird->y) {
                     a->angle = tempBird->angle + (rand() % 1 + 17) / 100; // выбор отклонения
@@ -367,35 +262,18 @@ int main()
             a->y1 = calcCoordinate(y1, y2);
 
         }
-        /*
-        if (p->thrust)  p->anim = sPlayer_go;
-        else   p->anim = sPlayer;
-        */
-        /*
-        for (auto e : entities)
-            if (e->name == "explosion")
-                if (e->anim.isEnd()) e->life = 0;
-     */
+
         for (auto i = entities.begin(); i != entities.end();)
         {
             Entity* e = *i;
-
             e->update();
             e->anim.update();
-          //  std::cout <<". "<< e->x1 << " " << e->y1 << "  ";
-
-           // if (e->life == false) { i = entities.erase(i); delete e; }
-          //  else
             i++;
         }
-       // std :: cout << std::endl;
         //////draw//////
         app.draw(background);
         for (auto i : entities) i->draw(app);
-       // std::ostringstream ss;
-       // ss << (entities.size() - 2);
-       //// text.setString(ss.str());
-        app.draw(text);
+     
         app.display();
     }
 
